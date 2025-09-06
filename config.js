3be686239d8537812ce93b5461b5caa04f85702f
Deeksha S J <deekshasj13@gmail.com>
2025-09-06 12:41:53 +0530

    Remove config.js from repo

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const firebaseConfig = {
-  apiKey: "AIzaSyB6CQDYdqt18j5qJr3MWgHVwYTBSrkclxo",
-  authDomain: "ecofinds-67cb8.firebaseapp.com",
-  projectId: "ecofinds-67cb8",
-  storageBucket: "ecofinds-67cb8.firebasestorage.app",
-  messagingSenderId: "929734148723",
-  appId: "1:929734148723:web:c2fcef4a671cba571a6006"
-};
